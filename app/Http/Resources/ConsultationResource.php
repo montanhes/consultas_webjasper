2c819cd0d8c7bf9c41512bb062ce45e1051addbe
Ramon Carvalho <ramon@oakz.org>
2025-10-31 20:00:09 -0300

    feat: added resources files

---
+++ app/Http/Resources/ConsultationResource.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ConsultationResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'user_id' => $this->user_id,
+            'title' => $this->title,
+            'scheduled_at' => $this->scheduled_at,
+            'status' => $this->status,
+            'total_price' => (float) $this->total_price,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+            'user' => new UserResource($this->whenLoaded('user')),
+            'services' => ServiceResource::collection($this->whenLoaded('services')),
+        ];
+    }
+}
